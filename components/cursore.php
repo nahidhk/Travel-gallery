bb8249ee1ed5a9d1405e33813e972fa099fa465c
nahidhk <nahidappltd@gmail.com>
2025-07-18 23:26:09 +0600

    new update

--- components/cursore.php
+++ components/cursore.php
@@ -1,15 +1,23 @@
-
+<?php
+$welcome_title = "Welcome to MIZAN TRAVEL GALLERY .: MTG Website";
+$welcome_note = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dicta non neque autem, eligendi voluptas excepturi vitae vero voluptatum ipsum consequatur id,"
+?>
 
 
 
 
 <div class="content">
-<div class="txbox">
-    <h1><?php echo $row['site_title'] ?></h1>
+    <div class="txbox">
+        <h1 class="modeltext"><?php echo $welcome_title ?></h1>
 
-    <p>
-        hello
-    </p>
+        <p class="text">
+            <?php echo $welcome_note ?>
+        </p>
+        <br><br><br>
+        <button class="button">
+            Click Me
+           <?php include 'button.php' ?>
+        </button>
 
-</div>
+    </div>
 </div>
