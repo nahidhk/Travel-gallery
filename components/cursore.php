0087aa6715a06cf9ad3ec82bc21fd67784ff8ab4
nahidhk <nahidappltd@gmail.com>
2025-07-18 17:23:05 +0600

    new update the markdown in text area

--- components/cursore.php
+++ components/cursore.php
@@ -1,4 +1,18 @@
 
+
+
+
+
 <div class="content">
-   
+<div class="txbox">
+    <h1><?php echo $row['site_title'] ?></h1>
+
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Assumenda dolorum deleniti consequatur quos omnis
+        cupiditate,
+        explicabo quod natus sit obcaecati aliquid nulla, illo harum quibusdam tenetur voluptates eos
+    </p>
+
+</div>
 </div>
--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,9 +1,8 @@
 function openDispaly(loadData) {
     const popup = document.getElementById(loadData.id);
     popup.classList = loadData.class;
-    popup.style.display = "flex"; // show popup explicitly
+    popup.style.display = "flex"; 
 
-    // Popup এর বাইরে ক্লিক করলে hide হবে
     function handler(e) {
         if (!popup.contains(e.target)) {
             popup.style.display = "none";
@@ -11,9 +10,9 @@ function openDispaly(loadData) {
         }
     }
 
-    // ইভেন্ট লিসেনার যোগ করো
+   
     setTimeout(() => {
-        // একটু delay দিয়ে যোগ করো, না হলে ওপেন বাটনের ক্লিক ইভেন্টও trigger হতে পারে
+       
         document.addEventListener("click", handler);
     }, 0);
 }
