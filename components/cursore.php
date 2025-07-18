81987c434e8d2f99ed94f63613b72d73313c492a
nahidhk <nahidappltd@gmail.com>
2025-07-18 17:42:26 +0600

    update cursore.php

--- components/cursore.php
+++ components/cursore.php
@@ -8,10 +8,6 @@
     <h1><?php echo $row['site_title'] ?></h1>
 
     <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Assumenda dolorum deleniti consequatur quos omnis
-        cupiditate,
-        explicabo quod natus sit obcaecati aliquid nulla, illo harum quibusdam tenetur voluptates eos
     </p>
 
 </div>
