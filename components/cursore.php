3bb6f5eb1a46ec76a4667f60f008fb8242be2aa4
nahidhk <nahidappltd@gmail.com>
2025-08-10 08:51:00 +0600

    new update

--- components/cursore.php
+++ components/cursore.php
@@ -1,12 +1,14 @@
 <?php
-$welcome_title = "Welcome to MIZAN TRAVEL GALLERY .: MTG Website";
-$welcome_note = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi dicta non neque autem, eligendi voluptas excepturi vitae vero voluptatum ipsum consequatur id,";
-$welcome_image = "https://www.gallerywallsmadeeasy.com/cdn/shop/collections/Gallery-Walls-Made-Easy-Travel_1400x.jpg?v=1654616583";
+$welcome_title = $row['welcome_title'];
+$welcome_note = $row['welcome_note'];
+$welcome_image = $row['welcome_image'];
 
 ?>
+<!-- sql command -->
 
 
 
+<!-- https://www.gallerywallsmadeeasy.com/cdn/shop/collections/Gallery-Walls-Made-Easy-Travel_1400x.jpg?v=1654616583 -->
 
 <div style="background-image: url('<?php echo $welcome_image ?>');" class="content">
     <div class="txbox">
